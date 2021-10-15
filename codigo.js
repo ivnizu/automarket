2373627529bd47dc307878f40de735d01c24377a
Iván <34077858+ivancil@users.noreply.github.com>
2021-10-14 23:52:29 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,25 +1,36 @@
 const botonEnviar = document.querySelector('#btn-enviar');
 
+
+function ValidarEmail(correoElectronico) 
+{
+    var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(correoElectronico) ? true : false;
+}
+
 let Validar = () => 
 {
     let inputMensajes = document.querySelectorAll('#mensajeContacto [required]');
-    let mensajeError = false;
+    let mensajeError = true;
+
+    var correoElectronico = document.getElementById('email').value;
+    
+    mensajeError = ValidarEmail(correoElectronico);
 
     for(i = 0; i < inputMensajes.length; i++)
     {
         if(inputMensajes[i].value == '')
         {
             inputMensajes[i].classList.add('errorVacio');
-            mensajeError = true;
+            mensajeError = false;
         }
         else
         {
             inputMensajes[i].classList.remove('errorVacio');
         }
     }
-
     return mensajeError;
 };
+
 
 let EnviarDatos = () =>
 {
@@ -27,17 +38,17 @@ let EnviarDatos = () =>
     if(mensajeError)
     {
         Swal.fire({
-            'title': 'ERROR',
-            'text':  'Complete correctamente los campos',
-            'icon': 'warning' 
+            'title': 'Éxito',
+            'text':  'Su mensaje ha sido enviado',
+            'icon': 'success' 
         });
     }
     else
     {
         Swal.fire({
-            'title': 'Éxito',
-            'text':  'Su mensaje ha sido enviado',
-            'icon': 'success' 
+            'title': 'ERROR',
+            'text':  'Complete correctamente los campos',
+            'icon': 'warning' 
         });
     }
 };
